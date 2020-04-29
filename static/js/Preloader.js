3935527f8a91402c35fb7569d5147e0f250c9cdd
Tejas <Tejas75O25@gmail.com>
2020-04-29 16:21:04 +0530

    Commit No5/ Added the Bunnies and Space Rocks( With Movement Enabled)

--- static/js/Preloader.js
+++ static/js/Preloader.js
@@ -14,8 +14,13 @@ BunnyDefender.Preloader.prototype = {
 		this.titleText = this.add.image(this.world.centerX, this.world.centerY-220, 'titleimage');
 		this.titleText.anchor.setTo(0.5, 0.5);
 		
-		this.load.image('titlescreen', 'static/images/TitleBG.png');
+		// Loading the Images and Fonts
+		this.load.image('titlescreen', 'static/images/TitleBG.png')
 		this.load.bitmapFont('eightbitwonder', 'static/fonts/eightbitwonder.png', 'static/fonts/eightbitwonder.fnt');
+		this.load.image('hill', 'static/images/hill.png');
+		this.load.image('sky', 'static/images/sky.png');
+		this.load.atlasXML('bunny', 'static/images/spritesheets/bunny.png', 'static/images/spritesheets/bunny.xml');
+		this.load.atlasXML('spacerock', 'static/images/spritesheets/SpaceRock.png', 'static/images/spritesheets/SpaceRock.xml');
 	},
 
 	create: function () {
