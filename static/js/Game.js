6c842248f4fe8adf089cbbadc2cf830c44876fe1
Tejas <Tejas75O25@gmail.com>
2020-05-01 07:43:40 +0530

    Commit No11/ Adding the first step to Game over( You cant fire any safety explosion nor will any more space rocks appear )

--- static/js/Game.js
+++ static/js/Game.js
@@ -5,10 +5,12 @@ BunnyDefender.Game = function(game) {
 	this.totalSpacerocks;
 	this.spacerockgroup;
 	this.burst;
+	this.gameover;
 };
 
 BunnyDefender.Game.prototype = {
 	create: function() {
+		this.gameover = false;
 		this.totalBunnies = 20;
 		this.totalSpacerocks = 13;
 		this.buildWorld();
@@ -84,8 +86,10 @@ BunnyDefender.Game.prototype = {
 	},
 
 	respawnRock: function(r) {
-		r.reset(this.rnd.integerInRange(0, this.world.width), this.rnd.realInRange(-1500, 0));
-		r.body.velocity.y = this.rnd.integerInRange(200, 400);
+		if (this.gameover == false) {
+			r.reset(this.rnd.integerInRange(0, this.world.width), this.rnd.realInRange(-1500, 0));
+			r.body.velocity.y = this.rnd.integerInRange(200, 400);	
+		}
 	},
 	
 	buildEmitter:function() {
@@ -99,9 +103,11 @@ BunnyDefender.Game.prototype = {
 	},
 	
 	fireBurst: function(pointer) {
-		this.burst.emitX = pointer.x;
-		this.burst.emitY = pointer.y;
-		this.burst.start(true, 1500, null, 20);
+		if (this.gameover == false) {
+			this.burst.emitX = pointer.x;
+			this.burst.emitY = pointer.y;
+			this.burst.start(true, 1500, null, 20);
+		}
 	},
 	
 	burstCollision: function(r, b) {
@@ -120,7 +126,7 @@ BunnyDefender.Game.prototype = {
 	
 	checkBunniesLeft: function() {
 		if (this.totalBunnies <= 0) {
-			// The Game Over Logic
+			this.gameover = true;
 		}
 	},
 	
